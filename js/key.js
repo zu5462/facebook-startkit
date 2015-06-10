b5f088c25fc35447773d2b3ae8f0456fe262f5fa
zu5462 <zu5462@gmail.com>
2015-06-10 19:47:35 +0800

    Update key.js

--- js/key.js
+++ js/key.js
@@ -1,7 +1,7 @@
 /*請貼上fackbook初始化的相關程式碼*/
 /*https://developers.facebook.com/*/
 
-<script>
+
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '863494857068848',
@@ -17,6 +17,6 @@
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-</script>
+
 
 
