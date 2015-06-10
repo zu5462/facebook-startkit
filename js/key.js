a24bddcd15598f8dd0f2e16d75f74ff965d09dd5
zu5462 <zu5462@gmail.com>
2015-06-10 19:41:42 +0800

    aa

--- js/key.js
+++ js/key.js
@@ -1,6 +1,22 @@
 /*請貼上fackbook初始化的相關程式碼*/
 /*https://developers.facebook.com/*/
 
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '863494857068848',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
 
